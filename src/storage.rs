a7d62410a503b1301c767ae491bba2ef9dfa555f
vladops <vlad123123v@gmail.com>
2024-07-01 04:28:13 +0300

    Create `STORAGE`

--- src/storage.rs
+++ src/storage.rs
@@ -1,9 +1,27 @@
 use serde;
 
+use crate::schemas::users::{User, UserAssets, UserOrders};
+use crate::schemas::assets::Asset;
+use crate::schemas::networks::{Network, NetworkTransactions};
+
+
 #[derive(serde::Deserialize, serde::Serialize, Debug, Clone)]
 pub struct Storage {
-    
+    pub users: Vec<User>,
+    pub users_assets: Vec<UserAssets>,
+    pub users_orders: Vec<UserOrders>,
+
+    pub assets: Vec<Asset>,
+
+    pub networks: Vec<Network>,
+    pub networks_txns: Vec<NetworkTransactions>,
 }
 
-
-pub static mut storage: Storage = Storage{};
+pub static mut STORAGE: Storage = Storage{
+    users: Vec::new(),
+    users_assets: Vec::new(),
+    users_orders: Vec::new(),
+    assets: Vec::new(),
+    networks: Vec::new(),
+    networks_txns: Vec::new(),
+};
