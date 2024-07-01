54c11fc9dec59d789e1733edfe9ac94871719ff9
vladops <vlad123123v@gmail.com>
2024-07-01 12:49:22 +0300

    Update workers

--- src/utils/url.rs
+++ src/utils/url.rs
@@ -1,13 +1,25 @@
 use std::collections::HashMap;
 
-pub fn build_url(
-        base_url: String,
-        endpoint: Option<String>,
-        params: Option<HashMap<String, String>>,
-) -> String {
-    let mut url: Vec<String> = vec![base_url];
 
-    if endpoint.is_some() {url.push(endpoint.unwrap())}
+pub fn build_url(
+    base_url: String,
+    endpoint: Option<String>,
+    params: Option<HashMap<String, String>>,
+) -> String {
+    let mut url: Vec<String> = vec![base_url.clone()];
+
+    if endpoint.is_some() {
+        let mut endpoint = endpoint.unwrap();
+
+        if base_url.as_str().ends_with("/") && endpoint.starts_with("/") {
+            endpoint = endpoint[1..endpoint.len()].to_string();
+
+            println!("there should be one slash between `base_url` and `endpoint`");
+            println!("removed slash from `endpoint`");
+        }
+
+        url.push(endpoint);
+    }
 
     if params.is_some() {
         let parameters: HashMap<String, String> = params.unwrap();
@@ -18,7 +30,9 @@ pub fn build_url(
         for (key, value) in parameters.iter() {
             url.push(format!("{}={}", key, value));
 
-            if parameters_length >= 2 {url.push("&".to_string())}
+            if parameters_length >= 2 {
+                url.push("&".to_string())
+            }
         }
     }
 
